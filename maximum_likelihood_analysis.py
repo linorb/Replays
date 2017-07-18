30b74d7f0208c8935f4be5c231e52a1d10437bc6
Linor BT <astrolinor@gmail.com>
2017-07-18 12:24:52 +0300

    Fixing the modules sources at maximum_likelihood_analysis

--- maximum_likelihood_analysis.py
+++ maximum_likelihood_analysis.py
@@ -1,8 +1,8 @@
 """Replay analysis - playground
 """
-import tools.matlab
-import tools.activity_loading
-import tools.maximum_likelihood
+import bambi.tools.matlab
+import bambi.tools.activity_loading
+import bambi.analysis.maximum_likelihood
 
 import matplotlib.pyplot as plt
 
@@ -10,27 +10,28 @@ events_filename = r'D:\data_for_analyzing_real_time_event_detector\c40m3_day1\ev
 frame_log_filename = r'D:\data_for_analyzing_real_time_event_detector\c40m3_day1\frameLog.csv'
 movment_filename = r'D:\data_for_analyzing_real_time_event_detector\c40m3_day1\behavioral.mat'
 
-events_mat = tools.matlab.load_events_file(events_filename)
-frames_indices = tools.matlab.load_frame_log_file(frame_log_filename)
-movment_data = tools.matlab.load_mvmt_file(movment_filename)
+events_mat = bambi.tools.matlab.load_events_file(events_filename)
+frames_indices = bambi.tools.matlab.load_frame_log_file(frame_log_filename)
+movment_data = bambi.tools.matlab.load_mvmt_file(movment_filename)
 
-events_trace = tools.activity_loading.order_events_into_trials(events_mat, frames_indices)
+events_trace = bambi.tools.activity_loading.order_events_into_trials(events_mat, frames_indices)
 
 # Train maximum likelihood decoder on all trials but one and test on the remaining
 # trial
-linear_trials_indices =[2,3,4,5]
-[train_bins1, train_events] = tools.activity_loading.create_training_data(movment_data, events_trace, linear_trials_indices)
+linear_trials_indices =[2,3,5,6]
+[train_bins1, train_events] = bambi.tools.activity_loading.create_training_data(movment_data, events_trace, linear_trials_indices)
 
-train_bins = tools.activity_loading.wide_binning(train_bins1,24, 3)
-p_neuron_bin = tools.maximum_likelihood.calculate_p_r_s_matrix(train_bins, train_events)
+train_bins = bambi.tools.activity_loading.wide_binning(train_bins1,24, 3)
+p_neuron_bin = bambi.analysis.maximum_likelihood.create_delayed_p_r_s(train_bins, train_events, 20)
 
-[test_bins1, test_events] = tools.activity_loading.create_training_data(movment_data, events_trace, [6])
-test_bins = tools.activity_loading.wide_binning(test_bins1,24, 3)
+[test_bins1, test_events] = bambi.tools.activity_loading.create_training_data(movment_data, events_trace, [4])
+test_bins = bambi.tools.activity_loading.wide_binning(test_bins1,24, 3)
 
-estimated_bins = tools.activity_loading.decode_entire_trial (test_events, p_neuron_bin)
+estimated_bins, estimated_prob = bambi.tools.activity_loading.decode_entire_trial (test_events, p_neuron_bin, 20)
 
-fig = plt.figure(1)
-ax = fig.add_subplot(111)
-ax.plot(test_bins,'r')
-ax.plot(estimated_bins[0],'b')
-ax.legend('Real bins','Estimated bins')
+fig, axx = plt.subplots(1,2)
+axx[0].plot(test_bins,'r')
+axx[0].plot(estimated_bins,'b')
+axx[1].plot(estimated_prob)
+fig.show()
+raw_input('press enter')
