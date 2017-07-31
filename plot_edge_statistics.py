7b050218eb142da4ae877f16e0eb203a8df951e8
Linor BT <astrolinor@gmail.com>
2017-07-31 13:47:52 +0300

    update edge statistics plotting

--- plot_edge_statistics.py
+++ plot_edge_statistics.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 def main():
     ##  Plot statistics of the linear track
-
+    f, axx = plt.subplots(2, 3)
     ######### For linear track #########:
 
     ### Load the data ###
-    filename = 'linear_edge_statistics.npz'
+    filename = r'results\linear_edge_statistics.npz'
     npzfile = np.load(filename)
     p_value = npzfile['p_value'].all()
     t_value = npzfile['t_value'].all()
@@ -27,79 +27,66 @@ def main():
                    (0.5, 0.5, 0)]
 
     ### Plot Cohen's D ###:
-    f0 = plt.figure()
     mouse_plot = []
     for i in range(len(MOUSE)):
         mouse_name = 'c%dm%d' %(CAGE[i], MOUSE[i])
-        a, = plt.plot(np.array(cohen_d[mouse_name]['d_before']),
+        a, = axx[0,2].plot(np.array(cohen_d[mouse_name]['d_before']),
              np.array(cohen_d[mouse_name]['d_after']),
              markerfacecolor=mouse_color[i], marker='o', linestyle='None',
                  markersize=8, label=mouse_name)
         mouse_plot.append(a)
 
-    plt.legend(handles=mouse_plot)
-    plt.plot(np.zeros(21), np.arange(-1, 1.1, 0.1), 'k')
-    plt.plot(np.arange(-1, 1.1, 0.1), np.zeros(21), 'k')
-    plt.grid()
-    plt.ylabel(
-        'Effect size of: p(active after run|active in run) - '
-        'p(active after run|not active in run)', fontsize=17)
-    plt.xlabel(
-        'Effect size of: p(active before run|active in run) -'
-        ' p(active before run|not active in run)', fontsize=17)
-    plt.title('Linear Track', fontsize=25)
-    plt.xticks(np.arange(-1, 1, 0.1), fontsize=15)
-    plt.yticks(np.arange(-1, 1, 0.1), fontsize=15)
-
-    f0.show()
+    # put the legend outside the axis. taken from:
+    #  https://matplotlib.org/users/legend_guide.html
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., handles=mouse_plot)
+    axx[0, 2].axhline(0, color='black')
+    axx[0, 2].axvline(0, color='black')
+    axx[0, 2].grid()
+    axx[0, 2].set_ylabel('after run', fontsize=17)
+    axx[0, 2].set_title('Effect size', fontsize=18)
+    axx[0, 2].set_xlim(-1, 1)
+    axx[0, 2].set_ylim(-1, 1)
 
     ### P value Vs t value ###
-    f, axx = plt.subplots(1, 2, sharex=True, sharey=True)
-    mouse_legend = []
+
     for i in range(len(MOUSE)):
         mouse_name = 'c%dm%d' % (CAGE[i], MOUSE[i])
-        axx[0].plot(np.array(t_value[mouse_name]['t_before']),
+        axx[0, 0].plot(np.array(t_value[mouse_name]['t_before']),
                   np.array(p_value[mouse_name]['p_before']),
                   markerfacecolor=mouse_color[i], marker='o', linestyle='None',
                   markersize=8)
 
-        a, = axx[1].plot(np.array(t_value[mouse_name]['t_after']),
-                         np.array(p_value[mouse_name]['p_after']),
-                         markerfacecolor=mouse_color[i], marker='o',
-                         linestyle='None', markersize=8, label=mouse_name)
-        mouse_legend.append(a)
+        axx[0, 1].plot(np.array(t_value[mouse_name]['t_after']),
+                     np.array(p_value[mouse_name]['p_after']),
+                     markerfacecolor=mouse_color[i], marker='o',
+                     linestyle='None', markersize=8, label=mouse_name)
 
-    plt.legend(handles=mouse_plot)
-    axx[0].axhline(0, color='black')
-    axx[0].axvline(0, color='black')
-    axx[0].axhline(0.025, color='red')
-    axx[0].grid()
-    axx[0].set_xlabel('T statistic', fontsize=17)
-    axx[0].set_ylabel('P value', fontsize=17)
-    axx[0].set_title('p(active before run|active in run) - \n'
-        'p(active before run|not active in run)')
+    axx[0, 0].axhline(0, color='black')
+    axx[0, 0].axvline(0, color='black')
+    axx[0, 0].axhline(0.025, color='red')
+    axx[0, 0].grid()
+    axx[0, 0].set_ylabel('Environment A \n\nP value', fontsize=17)
+    axx[0, 0].set_title('p(active before run|active in run) - \n'
+        'p(active before run|not active in run)', fontsize=18)
+    axx[0, 0].set_xlim(-10, 10)
+    axx[0, 0].set_ylim(-0.1, 1)
 
-    plt.xticks(np.arange(-10, 10, 1), fontsize=13)
-    plt.yticks(np.arange(0, 1.1, 0.1), fontsize=13)
-    axx[1].axhline(0, color='black')
-    axx[1].axvline(0, color='black')
-    axx[1].axhline(0.025, color='red')
-    axx[1].set_xlabel('T statistic', fontsize=17)
-    axx[1].set_ylabel('P value', fontsize=17)
-    axx[1].set_title('p(active after run|active in run) - \n'
-        'p(active after run|not active in run)')
-    axx[1].set_ylim(-0.01, 1.1)
-    axx[1].set_xlim(-10, 10)
-    axx[1].grid(axis='both')
-
-    f.suptitle('Linear Track', fontsize=25)
-
-    f.show()
+    axx[0, 1].axhline(0, color='black')
+    axx[0, 1].axvline(0, color='black')
+    axx[0, 1].axhline(0.025, color='red')
+    axx[0, 1].set_ylabel('P value', fontsize=17)
+    axx[0, 1].set_title('p(active after run|active in run) - \n'
+        'p(active after run|not active in run)', fontsize=18)
+    axx[0, 1].set_ylim(-0.01, 1.1)
+    axx[0, 1].set_xlim(-10, 10)
+    axx[0, 1].grid(axis='both')
+    axx[0, 1].set_xlim(-10, 10)
+    axx[0, 1].set_ylim(-0.1, 1)
 
     ######### For L-shape track #########:
 
     ### Load the data ###
-    filename = 'L-shape_edge_statistics.npz'
+    filename = r'results\L-shape_edge_statistics.npz'
     npzfile = np.load(filename)
     p_value = npzfile['p_value'].all()
     t_value = npzfile['t_value'].all()
@@ -113,73 +100,65 @@ def main():
                    (0.5, 0.5, 0)]
 
     ### Plot Cohen's D ###
-    f0 = plt.figure()
-    mouse_plot = []
     for i in range(len(MOUSE)):
         mouse_name = 'c%dm%d' % (CAGE[i], MOUSE[i])
-        a, = plt.plot(np.array(cohen_d[mouse_name]['d_before']),
+        axx[1, 2].plot(np.array(cohen_d[mouse_name]['d_before']),
                       np.array(cohen_d[mouse_name]['d_after']),
                       markerfacecolor=mouse_color[i], marker='o',
-                      linestyle='None',
-                      markersize=8, label=mouse_name)
-        mouse_plot.append(a)
+                      linestyle='None', markersize=8)
 
-    plt.legend(handles=mouse_plot)
-    plt.plot(np.zeros(21), np.arange(-1, 1.1, 0.1), 'k')
-    plt.plot(np.arange(-1, 1.1, 0.1), np.zeros(21), 'k')
-    plt.grid()
-    plt.ylabel(
-        'Effect size of: p(active after run|active in run) - '
-        'p(active after run|not active in run)', fontsize=17)
-    plt.xlabel(
-        'Effect size of: p(active before run|active in run) -'
-        ' p(active before run|not active in run)', fontsize=17)
-    plt.title('L-shape Track', fontsize=25)
-    plt.xticks(np.arange(-1, 1, 0.1), fontsize=15)
-    plt.yticks(np.arange(-1, 1, 0.1), fontsize=15)
+    axx[1, 2].axhline(0, color='black')
+    axx[1, 2].axvline(0, color='black')
+    axx[1, 2].grid()
+    axx[1, 2].set_ylabel(
+        'after run', fontsize=17)
+    axx[1, 2].set_xlabel(
+        'before run', fontsize=17)
+    axx[1, 2].set_xlim(-1, 1)
+    axx[1, 2].set_ylim(-1, 1)
 
-    # f0.show()
 
     ###  P value Vs t value ###
-    f, axx = plt.subplots(1, 2, sharex=True, sharey=True)
-    mouse_legend = []
     for i in range(len(MOUSE)):
         mouse_name = 'c%dm%d' % (CAGE[i], MOUSE[i])
-        axx[0].plot(np.array(t_value[mouse_name]['t_before']),
+        axx[1, 0].plot(np.array(t_value[mouse_name]['t_before']),
                   np.array(p_value[mouse_name]['p_before']),
                   markerfacecolor=mouse_color[i], marker='o', linestyle='None',
                   markersize=8)
 
-        a, = axx[1].plot(np.array(t_value[mouse_name]['t_after']),
+        axx[1, 1].plot(np.array(t_value[mouse_name]['t_after']),
                          np.array(p_value[mouse_name]['p_after']),
                          markerfacecolor=mouse_color[i], marker='o',
                          linestyle='None', markersize=8, label=mouse_name)
-        mouse_legend.append(a)
 
-    plt.legend(handles=mouse_plot)
-    axx[0].axhline(0, color='black')
-    axx[0].axvline(0, color='black')
-    axx[0].axhline(0.025, color='red')
-    axx[0].grid()
-    axx[0].set_xlabel('T statistic', fontsize=17)
-    axx[0].set_ylabel('P value', fontsize=17)
-    axx[0].set_title('p(active before run|active in run) - \n'
-        'p(active before run|not active in run)')
+    axx[1, 0].axhline(0, color='black')
+    axx[1, 0].axvline(0, color='black')
+    axx[1, 0].axhline(0.025, color='red')
+    axx[1, 0].grid()
+    axx[1, 0].set_xlabel('T statistic', fontsize=17)
+    axx[1, 0].set_ylabel('Environment B \n\nP value', fontsize=17)
+    # axx[1, 0].set_aspect('equal', 'datalim')
+    axx[1, 0].set_xlim(-10, 10)
+    axx[1, 0].set_ylim(-0.1, 1)
 
-    plt.xticks(np.arange(-10, 10, 1), fontsize=13)
-    plt.yticks(np.arange(0, 1.1, 0.1), fontsize=13)
-    axx[1].axhline(0, color='black')
-    axx[1].axvline(0, color='black')
-    axx[1].axhline(0.025, color='red')
-    axx[1].set_xlabel('T statistic', fontsize=17)
-    axx[1].set_ylabel('P value', fontsize=17)
-    axx[1].set_title('p(active after run|active in run) - \n'
-        'p(active after run|not active in run)')
-    axx[1].set_ylim(-0.01, 1.1)
-    axx[1].set_xlim(-10, 10)
-    axx[1].grid(axis='both')
+    axx[1,  1].axhline(0, color='black')
+    axx[1,  1].axvline(0, color='black')
+    axx[1,  1].axhline(0.025, color='red')
+    axx[1,  1].set_xlabel('T statistic', fontsize=17)
+    axx[1,  1].set_ylabel('P value', fontsize=17)
+    axx[1,  1].set_ylim(-0.01, 1.1)
+    axx[1,  1].set_xlim(-10, 10)
+    # axx[1,  1].set_aspect('equal', 'datalim')
+    axx[1,  1].grid(axis='both')
+    axx[1, 1].set_xlim(-10, 10)
+    axx[1, 1].set_ylim(-0.1, 1)
 
-    f.suptitle('L-shape Track', fontsize=25)
+    for i in range(2):
+        for j in range(3):
+            for xtick in axx[i, j].xaxis.get_major_ticks():
+               xtick.label.set_fontsize(15)
+            for ytick in axx[i, j].yaxis.get_major_ticks():
+               ytick.label.set_fontsize(15)
 
     f.show()
 
